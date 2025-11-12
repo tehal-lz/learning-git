686ad091d3f17d879bc2ffc8827e2125077be2b5
tehal-lz <tehalsingh@luminoguru.com>
2025-11-12 12:12:26 +0530

    delete

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log('App started')
 console.log('DB connection successful')
 console.log('Server running on port 3000')
+console.log('Server running on port 3000')
+console.log('Server running on port 3000')
